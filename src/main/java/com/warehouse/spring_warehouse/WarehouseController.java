5dcb96d62c622d091c65c4b0f62aefce0a5b58dc
jarossi1 <jarossi91@yahoo.com>
2024-09-19 14:29:21 -0400

    9/19/24 Add WarehouseController, form.html, and inventory.html - Added WarehouseController.java - Created form.html and inventory.html - Included CSS styles for both pages - Styled anchor links to function as buttons

--- src/main/java/com/warehouse/spring_warehouse/WarehouseController.java
+++ src/main/java/com/warehouse/spring_warehouse/WarehouseController.java
@@ -2,18 +2,30 @@ package com.warehouse.spring_warehouse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 
 /**
  * Handles the GET requests to the root URL ("/")
+ * @return the view of form (HTML page) 
  */
 @Controller
 public class WarehouseController {
     @GetMapping("/")
     public String getForm() {
         return "form";
+    }
+    
+    /**
+     * 
+     * @return the view of inventory (HTML page)
+     */
+    @GetMapping("/inventory")
+    public String getInventory() {
+        return "inventory";
     }
     
     
